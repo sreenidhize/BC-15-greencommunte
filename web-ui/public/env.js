4858941eff3d133709c68573c375dd0208c976bb
sreenidhze <srinidhi.gudise@zemosolabs.com>
2022-03-28 20:10:51 +0530

    file

--- web-ui/public/env.js
+++ web-ui/public/env.js
@@ -1,3 +1,3 @@
 window.env = Object.freeze({
-    BASE_URL: 'http://myrelease-backend:8080',
+    BASE_URL: 'http://a7a1dd967475342f4b10bbda5b441b3a-541439973.us-east-2.elb.amazonaws.com:8080',
 });
