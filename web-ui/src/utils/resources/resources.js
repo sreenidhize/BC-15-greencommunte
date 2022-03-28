556e8b3712236195ea2502b0536dc57eea3e5cce
sreenidhze <srinidhi.gudise@zemosolabs.com>
2022-03-28 19:03:14 +0530

    file

--- web-ui/src/utils/resources/resources.js
+++ web-ui/src/utils/resources/resources.js
@@ -1,4 +1,4 @@
-const { BASE_URL } = window.env || {BASE_URL : 'http://myrelease-backend:8080'};
+const { BASE_URL } = window.env || {BASE_URL : 'http://localhost:8080'};
 
 export const apis = {
   GET_SKILLS: `${BASE_URL}/api/skills`,
