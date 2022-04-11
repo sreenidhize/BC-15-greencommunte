09ef726529c88b4548237ec479f6e3e10d14e58c
sreenidhze <srinidhi.gudise@zemosolabs.com>
2022-04-11 10:59:17 +0530

    new-commit

--- web-ui/public/env.js
+++ web-ui/public/env.js
@@ -1,3 +1,3 @@
-// window.env = Object.freeze({
-//     BASE_URL: 'http://a4a48790e2daf40d2be98373a56063f0-696098843.us-east-2.elb.amazonaws.com:8080',
-// });
+window.env = Object.freeze({
+    BASE_URL: 'http://a4a48790e2daf40d2be98373a56063f0-696098843.us-east-2.elb.amazonaws.com:8080',
+});
